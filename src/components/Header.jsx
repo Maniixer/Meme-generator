bac0a9e4b33583418361862e91db7940b59d50c7
Maniixer <92598085+Maniixer@users.noreply.github.com>
2024-01-11 19:54:10 +0100

    moved img to public folder

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header>
       <div className="logo">
-        <img src="./src/assets/images/troll-face.png" alt="trollFace" className="logo-img" />
+        <img src="./troll-face.png" alt="trollFace" className="logo-img" />
         <h1 className="logo-text">Meme Generator</h1>
       </div>
       <div className="course">
